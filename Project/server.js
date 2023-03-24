21018aeeb0a65337d9b22cb7890c6b6601e00510
Gaurav Dhokchaule <86306108+gdhokchaule@users.noreply.github.com>
2023-03-24 11:53:37 +0530

    Project added

---
+++ Project/server.js
@@ -0,0 +1,118 @@
+  const express = require('express')
+  const path = require('path')
+  const multer = require('multer')
+  const mongoose = require('mongoose')
+  const bodyparser = require('body-parser')
+  const cloudinary = require('cloudinary').v2;
+  const fileUpload = require('express-fileupload')
+  
+//======================ALL Models imported=================================================
+
+  const app = express()
+  app.use(express.static('public'));
+  app.use(bodyparser.urlencoded({extended:false}));
+
+  app.set('views', './views')
+  app.set('view engine', 'ejs')
+
+  //=====================Connection to mongodb==================================================
+
+  mongoose.connect("mongodb+srv://Gauravd:gauravd@cluster0.hbhwc24.mongodb.net/?retryWrites=true&w=majority",{
+    useNewurlParser:true,
+    useUnifiedtopology:true
+  }).then(()=>{
+    console.log("Connected With MongoDB");
+}).catch((err)=>{
+  console.log(`error is ${err}`);
+})
+
+const imageSchema = new mongoose.Schema({
+  url: String,
+});
+const Image = mongoose.model('Image', imageSchema);
+
+
+  app.use(express.json());
+
+  //======================Connection to Cloudinary=================================================
+
+  cloudinary.config({
+    cloud_name:'ds197oik9',
+    api_key:'592329952254324',
+    api_secret:'R_qJLiZmZzCoC3m4j3X66UJbZyo',
+  });
+
+  app.use(fileUpload({
+    useTempFiles: true,
+    limits: { fileSize: 100 * 1024 * 1024 }  }));
+
+  
+  //======================Posting the Image into cloudinary and url into DB API=================================================
+
+  app.post('/action', async (req, res) => {
+    console.log(req.body);
+    const file = req.files.image;
+    try {
+      const result = await cloudinary.uploader.upload(file.tempFilePath);
+      console.log(result);
+      const image = new Image({
+        url: result.url
+      });
+      await image.save();
+      // res.sendFile(__dirname + '/views/index.ejs');
+    const images = await Image.find();
+    const imageUrls = images.map(image => image.url);
+      res.render('index',{imageUrls});
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error saving image');
+    }
+  });
+
+  //======================Fetching the Image url from DB API=================================================
+
+  app.get('/action/:id', async (req, res) => {
+    const image = await Image.findById(req.params.id);
+    if (!image) {
+      return res.status(404).send('Image not found');
+    }
+    const imageUrl = image.url;
+    res.render('index', { imageUrl });
+  });
+
+  //=====================Main EJS File==================================================
+  
+  app.get('/home', async (req, res) => {
+    const images = await Image.find();
+    const imageUrls = images.map(image => image.url);
+    res.render('index', { imageUrls });
+  });
+  
+  //=====================Uploading Image==================================================
+
+  app.get('/getupload', (req, res) => {
+    res.sendFile(__dirname + "/upload.html");
+  })
+
+  //=======================================================================
+
+    app.get('/view', (req, res) => {
+      res.sendFile(__dirname + "/views/index.ejs");
+  
+    });
+   
+  //=======================================================================
+
+    app.get("/story1", (req, res) => {
+      res.sendFile(__dirname + "/upload.html");
+  })
+
+
+  
+  
+  app.listen(3000, () => {
+    console.log(`server running`)
+  })
+
+
+
---
+++ Project/upload.js
@@ -0,0 +1,15 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://localhost:27017/Takesback",{useNewurlParser:true,useUnifiedtopology:true}).then(()=>{
+//     console.log("Connected With MongoDB");
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// const uploadSchema = new mongoose.Schema({
+//     imagename: {type:String,require:true},
+// });
+
+// const Upload = mongoose.model('UploadImage', uploadSchema);
+// module.exports=Upload;
+  
